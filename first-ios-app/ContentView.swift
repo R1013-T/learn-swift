a0119eaca4bf5c3c31538d4e232e79c480171185
R1013-T <takahashi.ryunosuke.job@gmail.com>
2024-12-27 00:23:49 +0900

    Initial Commit

---
+++ first-ios-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  first-ios-app
+//
+//  Created by 髙橋龍之介 on 2024/12/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ first-ios-app/first_ios_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  first_ios_appApp.swift
+//  first-ios-app
+//
+//  Created by 髙橋龍之介 on 2024/12/27.
+//
+
+import SwiftUI
+
+@main
+struct first_ios_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
